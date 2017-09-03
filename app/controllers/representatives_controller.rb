e8ad0d6db857836be72bafd61149c72754615a7a
Vasily Toropchin <vasview@gmail.com>
2017-09-03 21:23:54 +0600

    #5 Created CRUD for Representatives.

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -13,16 +13,36 @@ class RepresentativesController < ApplicationController
   end
 
   def create
-    binding.pry
+    @representative = Representative.create(representative_params.merge(fullname: representative_fullname))
+    if @representative.save
+
+      redirect_to representatives_path, notice: "Карточка представителя компании успешно добавлен."
+    else
+      render :new
+    end
   end
 
   def edit
+    @representative = Representative.find(params[:id])
   end
 
   def update
+    @representative = Representative.find(params[:id])
+
+    if @representative.update_attributes(representative_params.merge(fullname: representative_fullname))
+
+      redirect_to representative_path(@representative), notice: "Карточка представителя компании успешно отредактирована."
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @representative = Representative.find(params[:id])
+
+    @representative.destroy
+
+    redirect_to representatives_path, notice: "Карточка представителя компании удалена."
   end
 
   private
@@ -31,5 +51,11 @@ class RepresentativesController < ApplicationController
     params.require(:representative).permit(:id, :firstname, :middlename, :lastname,
                                            :work_phone, :mobile_phone, :email, :company_id,
                                            :job_position_id, :birthdate, :notes)
+  end
+
+  def representative_fullname
+    [ params[:representative][:firstname],
+      params[:representative][:middlename],
+      params[:representative][:lastname] ].join(' ')
   end
 end
