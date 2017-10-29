f615c1ccfd979935f7e792eb76ac64456c9d1fb6
Vasily Toropchin <vasview@gmail.com>
2017-10-29 13:00:21 +0600

    #25 Changed the acton from get_filtered_company to filter. Add icons for active and inactive companies. Added filters for inactive and active companies.

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -9,24 +9,13 @@ class CompaniesController < ApplicationController
     @representative = @executive.build_representative
   end
 
-  # def index
-  #   @companies = Company.status('active').all
-
-  #   set_filter_period
-
-  #   score_companies(@companies)
-
-  #   @corporate_members = get_corporate_members
-  #   @associates_members = get_accossiates_members
-  #   @honarable_members = get_honarable_members
-  # end
-
   def index
-    @active_companies = Company.status('active').paginate(page: params[:page], per_page: "20")
-    @inactive_companies = Company.status('inactive').all
-    @corporate_members = get_corporate_members
-    @associates_members = get_accossiates_members
-    @honarable_members = get_honarable_members
+    @companies = Company.all.paginate(page: params[:page], per_page: "20")
+    @active_companies = Company.status('active').size
+    @inactive_companies = Company.status('inactive').size
+    @corporate_members = helpers.get_corporate_members
+    @associates_members = helpers.get_accossiates_members
+    @honarable_members = helpers.get_honarable_members
   end
 
   def show
@@ -79,16 +68,10 @@ class CompaniesController < ApplicationController
     redirect_to companies_path, notice: "Карточка компании успешно удалена."
   end
 
-  def get_filtered_companies
+  def filter
     set_filter_period
 
-    @companies = Company.status('active').filter(period: @filter_period).filter(params[:filter].slice(:category))
-
-    score_companies(@companies)
-
-    if color_filters.present?
-     @companies = filter_by_colors(@companies, color_filters)
-    end
+    @companies = Company.filter(period: @filter_period).filter(params[:filter].slice(:category, :status))
 
   end
 
@@ -115,58 +98,6 @@ class CompaniesController < ApplicationController
 
   def filter_params
     return {period: ''} if params[:filter].nil?
-    params.require(:filter).permit(:category, :date, :period, colors: [])
-  end
-
-  def color_filters
-    params[:filter][:colors]
-  end
-
-  def score_companies(companies)
-    companies.each do |company|
-      company.total = get_scores(company)
-
-      set_company_color(company)
-    end
-    companies
-  end
-
-  def get_scores(company)
-    marks = []
-    interaction_results = company.interaction_results.period(@filter_period)
-    interaction_results.map{|result| marks << result.mark }
-    total = marks.inject(0, :+)
-  end
-
-  def set_company_color(company)
-    if company.total > 4
-      company.color = "green"
-    elsif company.total >= 1
-      company.color = "yellow"
-    else
-      company.color = "red"
-    end
-  end
-
-  def filter_by_colors(companies, colors)
-    companies.select{ |company| colors.include?(company.color) }
-  end
-
-  def get_corporate_members
-    total_corporate_members = 0
-    (1..4).each do |i|
-      total_corporate_members += Category.find(i).companies.size
-    end
-    total_corporate_members
-  end
-
-  def get_accossiates_members
-    associates_category_id = Category.where(title: 'Ассоциативный член').ids
-    Category.find(associates_category_id.first).companies.size
-  end
-
-  def get_honarable_members
-    honorable_category_id = Category.where(title: 'Почетный член').ids
-    Category.find(honorable_category_id.first).companies.size
+    params.require(:filter).permit(:category, :date, :period, :status)
   end
 end
