d7c5f00f6d8799bb2f46a65f18405d5d24afcd24
Vasily Toropchin <vasview@gmail.com>
2018-01-08 00:41:40 +0600

    #35 CRUD for Industries was created. Industries could be managed by admin user.

---
+++ app/controllers/admin/industries_controller.rb
@@ -0,0 +1,51 @@
+class Admin::IndustriesController < Admin::BaseController
+  def index
+    @industry_all = Industry.all.order(:title)
+  end
+
+  def new
+    @industry = Industry.new
+  end
+
+  def create
+    @industry = Industry.create(industry_params)
+
+    if @industry
+      redirect_to admin_industries_path, notice: "Сектор экономики успешно добавлен."
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @industry = Industry.find(params[:id])
+  end
+
+  def update
+    @industry = Industry.find(params[:id])
+
+    if @industry.update_attributes(industry_params)
+      flash[:notice] = "Карточка сектора экономики успешно отредактирована"
+
+      redirect_to admin_industries_path, notice: "Карточка сектора экономики успешно обновлена."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @industry = Industry.find(params[:id])
+
+    if @industry.update_attribute('status', 'archived')
+      redirect_to admin_industries_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def industry_params
+    params.require(:industry).permit(:id, :title, :status)
+  end
+end
